646374156be38f28f8224af3d0b0c64c0d8f42a9
rightjoint <rightjoint@yandex.ru>
2023-09-20 10:53:38 +0300

    model_main-lang_map

--- application/models/model_main.php
+++ application/models/model_main.php
@@ -1,8 +1,15 @@
 <?php
 class Model_Main extends Model
 {
+    public $lang_map = array(
+        "main_page_text" => array(
+            "en" => "Thanks for use my software",
+            "rus" => "Спасибо что пользуетесь моими программами"
+        )
+    );
+
     function get_data()
     {
-        return "Thanks for use my products";
+        return $this->lang_map["main_page_text"][$_SESSION["lang"]];
     }
 }
